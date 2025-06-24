af9861b40d310e0b7b7404063cfcb2ec72b9dbe8
Nguyễn Hoàng Anh <160804973+hiroshimayuk@users.noreply.github.com>
2025-06-24 13:05:06 +0700

    sửa file để hiển thị đầy đủ

--- BookinhMVC/Models/MedicalRecordViewModel.cs
+++ BookinhMVC/Models/MedicalRecordViewModel.cs
@@ -5,6 +5,7 @@ namespace BookinhMVC.Models
         public int MaHoSo { get; set; }
         public string TenBenhNhan { get; set; }
         public string TenBacSi { get; set; }
+        public int MaBacSi { get; set; }
         public DateTime NgayKham { get; set; }
         public string ChanDoan { get; set; }
         public string PhuongAnDieuTri { get; set; }
